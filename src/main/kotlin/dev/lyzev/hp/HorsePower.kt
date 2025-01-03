576cc9665cf4dedbb4ffd09cd43068036e091960
Lyzev <lyzev.info@gmail.com>
2025-01-03 20:47:25 +0100

    Add stats command and update search feedback messages for horses.

--- src/main/kotlin/dev/lyzev/hp/HorsePower.kt
+++ src/main/kotlin/dev/lyzev/hp/HorsePower.kt
@@ -19,6 +19,9 @@ package dev.lyzev.hp
 
 import com.mojang.brigadier.context.CommandContext
 import dev.lyzev.hp.modmenu.HorsePowerConfigManager
+import dev.lyzev.hp.util.round
+import dev.lyzev.hp.util.unit2bps
+import dev.lyzev.hp.util.unit2jump
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
@@ -48,27 +51,64 @@ object HorsePower : ClientModInitializer {
 
         ClientCommandRegistrationCallback.EVENT.register(ClientCommandRegistrationCallback { dispatcher, _ ->
             dispatcher.register(
-                ClientCommandManager.literal("search")
-                    .executes { context: CommandContext<FabricClientCommandSource> ->
-                        val horses = mc.world!!.entities.filter { it is HorseEntity || it is DonkeyEntity || it is MuleEntity }.sortedBy {
-                            val horse = it as AbstractHorseEntity
-                            val movementSpeed = horse.getAttributeBaseValue(EntityAttributes.MOVEMENT_SPEED).coerceIn(AbstractHorseEntity.MIN_MOVEMENT_SPEED_BONUS.toDouble(), AbstractHorseEntity.MAX_MOVEMENT_SPEED_BONUS.toDouble()) / AbstractHorseEntity.MAX_MOVEMENT_SPEED_BONUS.toDouble()
-                            val jumpStrength = horse.getAttributeBaseValue(EntityAttributes.JUMP_STRENGTH).coerceIn(AbstractHorseEntity.MIN_JUMP_STRENGTH_BONUS.toDouble(), AbstractHorseEntity.MAX_JUMP_STRENGTH_BONUS.toDouble()) / AbstractHorseEntity.MAX_JUMP_STRENGTH_BONUS.toDouble()
-                            val health = horse.getAttributeBaseValue(EntityAttributes.MAX_HEALTH).coerceIn(AbstractHorseEntity.MIN_HEALTH_BONUS.toDouble(), AbstractHorseEntity.MAX_HEALTH_BONUS.toDouble()) / AbstractHorseEntity.MAX_HEALTH_BONUS.toDouble()
-                            movementSpeed + jumpStrength + health
-                        }
-                        if (horses.isEmpty()) {
-                            context.source.sendError(Text.translatable("horsepower.search.error"))
-                            0
-                        } else {
-                            last = System.currentTimeMillis()
-                            this.horses.clear()
-                            this.horses += horses.take(2)
-                            context.source.sendFeedback(Text.translatable(if (horses.count() == 1) "horsepower.search.success.one" else "horsepower.search.success.two").withColor(Formatting.GREEN.colorValue!!))
-                            1
-                        }
+                ClientCommandManager.literal("search").executes { context: CommandContext<FabricClientCommandSource> ->
+                    val horses =
+                        mc.world!!.entities.filter { it is HorseEntity || it is DonkeyEntity || it is MuleEntity }
+                            .sortedBy {
+                                val horse = it as AbstractHorseEntity
+                                val movementSpeed =
+                                    horse.getAttributeBaseValue(EntityAttributes.MOVEMENT_SPEED).coerceIn(
+                                        AbstractHorseEntity.MIN_MOVEMENT_SPEED_BONUS.toDouble(),
+                                        AbstractHorseEntity.MAX_MOVEMENT_SPEED_BONUS.toDouble()
+                                    ) / AbstractHorseEntity.MAX_MOVEMENT_SPEED_BONUS.toDouble()
+                                val jumpStrength = horse.getAttributeBaseValue(EntityAttributes.JUMP_STRENGTH).coerceIn(
+                                    AbstractHorseEntity.MIN_JUMP_STRENGTH_BONUS.toDouble(),
+                                    AbstractHorseEntity.MAX_JUMP_STRENGTH_BONUS.toDouble()
+                                ) / AbstractHorseEntity.MAX_JUMP_STRENGTH_BONUS.toDouble()
+                                val health = horse.getAttributeBaseValue(EntityAttributes.MAX_HEALTH).coerceIn(
+                                    AbstractHorseEntity.MIN_HEALTH_BONUS.toDouble(),
+                                    AbstractHorseEntity.MAX_HEALTH_BONUS.toDouble()
+                                ) / AbstractHorseEntity.MAX_HEALTH_BONUS.toDouble()
+                                movementSpeed + jumpStrength + health
+                            }
+                    if (horses.isEmpty()) {
+                        context.source.sendError(Text.translatable("horsepower.search.error"))
+                        0
+                    } else {
+                        last = System.currentTimeMillis()
+                        this.horses.clear()
+                        this.horses += horses.take(2)
+                        context.source.sendFeedback(
+                            Text.translatable(if (horses.count() == 1) "horsepower.search.success.one" else "horsepower.search.success.two")
+                                .withColor(Formatting.GREEN.colorValue!!)
+                        )
+                        1
                     }
-            )
+                })
+            // TODO: Add /stats command that gets crosshair entity check if it's a horse and then display stats
+            dispatcher.register(
+                ClientCommandManager.literal("stats").executes { context: CommandContext<FabricClientCommandSource> ->
+                    val targetEntity = mc.targetedEntity
+                    if (targetEntity is AbstractHorseEntity) {
+                        val movementSpeed = targetEntity.getAttributeBaseValue(EntityAttributes.MOVEMENT_SPEED)
+                        val jumpStrength = targetEntity.getAttributeBaseValue(EntityAttributes.JUMP_STRENGTH)
+                        val health = targetEntity.getAttributeBaseValue(EntityAttributes.MAX_HEALTH)
+                        context.source.sendFeedback(
+                            Text.translatable(
+                                "horsepower.stats.success",
+                                unit2bps(movementSpeed).round(1),
+                                unit2jump(jumpStrength).round(1),
+                                health.round(1)
+                            ).formatted(Formatting.GREEN)
+                        )
+                        1
+                    } else {
+                        context.source.sendError(
+                            Text.translatable("horsepower.stats.error").formatted(Formatting.RED)
+                        )
+                        0
+                    }
+                })
         })
     }
 }
