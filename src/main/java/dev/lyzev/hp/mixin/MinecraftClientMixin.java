2c14d4d9098c362485603faaaf09fa569367b284
Lyzev <69327579+Lyzev@users.noreply.github.com>
2025-04-19 15:11:13 +0200

    fix

---
+++ src/main/java/dev/lyzev/hp/mixin/MinecraftClientMixin.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025. Lyzev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.lyzev.hp.mixin;
+
+import dev.lyzev.hp.client.HorsePowerClient;
+import kotlin.Metadata;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.AbstractHorseEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftClient.class)
+public final class MinecraftClientMixin {
+   @Inject(method = "hasOutline", at = @At("HEAD"), cancellable = true)
+   private void onHasOutline(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+      if (entity instanceof AbstractHorseEntity && System.currentTimeMillis() - HorsePowerClient.INSTANCE.getLast() <= 5000L && HorsePowerClient.INSTANCE.getHorses().contains(entity)) {
+         cir.setReturnValue(true);
+         cir.cancel();
+      }
+   }
+}
